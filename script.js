c8ae55394dcff5ab5b3329355d04e8922e6096f4
Eternum101 <eternumhd111@gmail.com>
2023-06-12 09:55:32 +1000

    Modified Header Styling in style.css Added Comments

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
+// Declaring variables
 const books = document.querySelector('.grid-container');
 const addBook = document.querySelector('.btn-add');
 const modal = document.getElementById('modal');
 const span = document.querySelector('.close');
 const addBookForm = document.querySelector('.add-book-form'); 
 
+// Adds a click event listener to "addBook" element and displays a modal, 
+// updates form title to "Add New Book" and submit button text to "Submit"
 addBook.addEventListener('click', () => {
     modal.style.display = 'block';
     document.querySelector('.form-title').textContent = "Add New Book";
     document.querySelector('.form-submit-button').textContent = "Submit";
 });
 
+// Handles form submission, creates/updates a book object based on form data, 
+// saves and renders books if editing, adds new book to library if not editing, 
+// resets form, and hides modal
 addBookForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const data = new FormData(e.target);
@@ -52,10 +58,13 @@ window.addEventListener('click', (e) => {
     if(e.target === modal) {
         modal.style.display = "none";
     }
-})
+});
 
+// Declares an empty array named "myLibrary" to store book objects
 let myLibrary = [];
 
+// Constructor function for creating a Book object with title, author, pages, 
+// read, and a random id 
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -64,11 +73,15 @@ function Book(title, author, pages, read) {
     this.id = Math.floor(Math.random() *1000000000);
 }
 
+// Adds a new Book object with the provided title, author, pages, 
+// and read values to the myLibrary array, and then saves and renders the books
 function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read));
     saveAndRenderBooks();
 }
 
+// Creates and returns a new HTML element (el) with the provided content 
+// and class name (className)
 function createBookElement(el, content, className) {
     const element = document.createElement(el);
     element.textContent = content;
@@ -76,6 +89,8 @@ function createBookElement(el, content, className) {
     return element;
 }
 
+// Creates and returns a new read element for a book item, including a title, 
+// checkbox, and event listener to toggle the read status of the book
 function createReadElement(bookItem, book) {
     const read = document.createElement('div');
     read.setAttribute('class', 'book-read');
@@ -101,21 +116,29 @@ function createReadElement(bookItem, book) {
     return read;
 }
 
+// Retrieves 'library' data from local storage and assigns it to myLibrary array, 
+// or initializes an empty array, saves and renders the books
 function addLocalStorage() {
     myLibrary = JSON.parse(localStorage.getItem('library')) || [];
     saveAndRenderBooks(); 
 }
 
+// Saves the myLibrary array to local storage as 'library' data in JSON format, 
+// and then renders the books
 function saveAndRenderBooks () {
     localStorage.setItem('library', JSON.stringify(myLibrary));
     renderBooks();
 }
 
+// Removes a book from the myLibrary array at the specified index
 function deleteBook(index) {
     myLibrary.splice(index, 1);
     saveAndRenderBooks();
 }
 
+// Fills out the edit form with the details of the provided book object, 
+// updates the modal display, form title, submit button text, form id, 
+// and input values accordingly
 function fillOutEditForm(book) {
     modal.style.display = 'block';
     document.querySelector('.form-title').textContent = 'Edit Book';
@@ -127,6 +150,8 @@ function fillOutEditForm(book) {
     document.getElementById('book-read').checked = book.read;
 }
 
+// Creates and returns an edit icon element for a book, with a click event listener 
+// that fills out the edit form with the details of the corresponding book
 function createEditIcon(book) {
     const editIcon = document.createElement('span');
     editIcon.setAttribute('class', 'material-symbols-outlined');
@@ -138,6 +163,10 @@ function createEditIcon(book) {
     return editIcon;
 }
 
+// Creates and returns a book item element for a book, with various attributes, 
+// child elements for title, author, pages, read status, delete button, and edit icon. 
+// Also adds event listener for delete button click to remove the corresponding book 
+// from the library
 function createBookItem(book, index) {
     const bookItem = document.createElement('div');
     bookItem.setAttribute('id', index);
@@ -153,6 +182,8 @@ function createBookItem(book, index) {
     books.insertAdjacentElement('afterbegin', bookItem);
 }
 
+// Clears the content of the books element. Then, for each book in myLibrary, 
+// calls the createBookItem function to create and display a book item element
 function renderBooks () {
     books.textContent = "";
     myLibrary.map((book, index) => {
@@ -160,4 +191,5 @@ function renderBooks () {
     });
 }
 
+// Calls the addLocalStorage function
 addLocalStorage();
